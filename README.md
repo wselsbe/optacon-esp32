ca9ab5cd43dfe580a2b3497fb25f2a720801de5e
Wannes Sels <wannes@wsels.be>
2023-08-17 16:01:06 +0200

    i2c working

--- README.md
+++ README.md
@@ -1,52 +1,52 @@
-# Hello World Example
+| Supported Targets | ESP32 | ESP32-C3 | ESP32-C6 | ESP32-H2 | ESP32-S2 | ESP32-S3 |
+| ----------------- | ----- | -------- | -------- | -------- | -------- | -------- |
 
-Starts a FreeRTOS task to print "Hello World".
+# I2C Simple Example
 
 (See the README.md file in the upper level 'examples' directory for more information about examples.)
 
+## Overview
+
+This example demonstrates basic usage of I2C driver by reading and writing from a I2C connected sensor:
+
+If you have a new I2C application to go (for example, read the temperature data from external sensor with I2C interface), try this as a basic template, then add your own code.
+
 ## How to use example
 
-Follow detailed instructions provided specifically for this example. 
+### Hardware Required
 
-Select the instructions depending on Espressif chip installed on your development board:
+To run this example, you should have one ESP32, ESP32-S, ESP32-C or ESP32-H based development board as well as a MPU9250. MPU9250 is a inertial measurement unit, which contains a accelerometer, gyroscope as well as a magnetometer, for more information about it, you can read the [datasheet of the MPU9250 sensor](https://invensense.tdk.com/wp-content/uploads/2015/02/PS-MPU-9250A-01-v1.1.pdf).
 
-- [ESP32 Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/stable/get-started/index.html)
-- [ESP32-S2 Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/latest/esp32s2/get-started/index.html)
+#### Pin Assignment:
+
+**Note:** The following pin assignments are used by default, you can change these in the `menuconfig` .
+
+|                  | SDA             | SCL           |
+| ---------------- | -------------- | -------------- |
+| ESP I2C Master   | I2C_MASTER_SDA | I2C_MASTER_SCL |
+| MPU9250 Sensor   | SDA            | SCL            |
 
 
-## Example folder contents
+For the actual default value of `I2C_MASTER_SDA` and `I2C_MASTER_SCL` see `Example Configuration` in `menuconfig`.
 
-The project **hello_world** contains one source file in C language [hello_world_main.c](main/hello_world_main.c). The file is located in folder [main](main).
+**Note:** There's no need to add an external pull-up resistors for SDA/SCL pin, because the driver will enable the internal pull-up resistors.
 
-ESP-IDF projects are built using CMake. The project build configuration is contained in `CMakeLists.txt` files that provide set of directives and instructions describing the project's source files and targets (executable, library, or both). 
+### Build and Flash
 
-Below is short explanation of remaining files in the project folder.
+Enter `idf.py -p PORT flash monitor` to build, flash and monitor the project.
 
+(To exit the serial monitor, type ``Ctrl-]``.)
+
+See the [Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/latest/get-started/index.html) for full steps to configure and use ESP-IDF to build projects.
+
+## Example Output
+
+```bash
+I (328) i2c-simple-example: I2C initialized successfully
+I (338) i2c-simple-example: WHO_AM_I = 71
+I (338) i2c-simple-example: I2C de-initialized successfully
 ```
-├── CMakeLists.txt
-├── example_test.py            Python script used for automated example testing
-├── main
-│   ├── CMakeLists.txt
-│   ├── component.mk           Component make file
-│   └── hello_world_main.c
-├── Makefile                   Makefile used by legacy GNU Make
-└── README.md                  This is the file you are currently reading
-```
-
-For more information on structure and contents of ESP-IDF projects, please refer to Section [Build System](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html) of the ESP-IDF Programming Guide.
 
 ## Troubleshooting
 
-* Program upload failure
-
-    * Hardware connection is not correct: run `idf.py -p PORT monitor`, and reboot your board to see if there are any output logs.
-    * The baud rate for downloading is too high: lower your baud rate in the `menuconfig` menu, and try again.
-
-## Technical support and feedback
-
-Please use the following feedback channels:
-
-* For technical queries, go to the [esp32.com](https://esp32.com/) forum
-* For a feature request or bug report, create a [GitHub issue](https://github.com/espressif/esp-idf/issues)
-
-We will get back to you as soon as possible.
+(For any technical queries, please open an [issue](https://github.com/espressif/esp-idf/issues) on GitHub. We will get back to you as soon as possible.)
--- main/motherboard_main.c
+++ main/motherboard_main.c
@@ -7,17 +7,20 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include "esp_log.h"
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_log.h"
 #include "driver/spi_master.h"
 #include "driver/i2c.h"
-#include "driver/spi_master.h"
 #include <math.h>
 
+
+static const char *TAG = "motherboard";
+
 #define I2C_MASTER_NUM 0
+#define I2C_MASTER_CLK 400000
 #define I2C_SCL_PIN   21
 #define I2C_SDA_PIN   47
 
@@ -75,7 +78,8 @@ void generate_sine_wave(float frequency) {
     for (size_t i = 0; i < sine_wave_size; i++) {
         float value = sin(phase);
         int twos_comp_value = (int)(value * 127);
-        sine_wave[i] = (uint8_t)(twos_comp_value + 0x80);
+        sine_wave[i] = (uint8_t)(twos_comp_value < 0 ? twos_comp_value + 0xFF : twos_comp_value);
+        ESP_LOGI(TAG, "sine_wave[%i]=%x",i,sine_wave[i]);
 
         phase += sine_wave_step;
         if (phase >= 2 * M_PI) {
@@ -88,52 +92,66 @@ void drv_init() {
     i2c_config_t i2c_conf;
     i2c_conf.mode = I2C_MODE_MASTER;
     i2c_conf.sda_io_num = I2C_SDA_PIN;
-    i2c_conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
+    i2c_conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
     i2c_conf.scl_io_num = I2C_SCL_PIN;
-    i2c_conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
-    i2c_conf.master.clk_speed = 100000;
+    i2c_conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    i2c_conf.master.clk_speed = I2C_MASTER_CLK;
+    i2c_conf.clk_flags = 0;
 
     i2c_param_config(I2C_MASTER_NUM, &i2c_conf);
     ESP_ERROR_CHECK(i2c_driver_install(I2C_MASTER_NUM, i2c_conf.mode, 0, 0, 0));
 
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    
+    vTaskDelay((5)/portTICK_PERIOD_MS);
+
     i2c_cmd_handle_t i2c_cmd = i2c_cmd_link_create();
     i2c_master_start(i2c_cmd);
-    i2c_master_write_byte(i2c_cmd, (DRV2665_ADDR < 1 ) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(i2c_cmd, (DRV2665_ADDR<<1)| I2C_MASTER_WRITE, true);
     i2c_master_write_byte(i2c_cmd, DRV_REGISTER_2, true);
     i2c_master_write_byte(i2c_cmd, DRV_RESET, true);
     i2c_master_stop(i2c_cmd);
-    ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_MASTER_NUM, i2c_cmd, pdMS_TO_TICKS(5000)));
+    ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_MASTER_NUM, i2c_cmd, pdMS_TO_TICKS(5)));
     i2c_cmd_link_delete(i2c_cmd);
 }
 
 void drv_write_fifo(uint8_t* data, size_t data_len) {
     i2c_cmd_handle_t i2c_cmd = i2c_cmd_link_create();
     i2c_master_start(i2c_cmd);
-    i2c_master_write_byte(i2c_cmd, (DRV2665_ADDR << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(i2c_cmd, (DRV2665_ADDR<<1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(i2c_cmd, DRV_REGISTER_DATA, true);
     i2c_master_write(i2c_cmd, data, data_len, true);
-    i2c_master_stop(i2c_cmd);
+    i2c_master_stop(i2c_cmd);    
+    i2c_master_cmd_begin(I2C_MASTER_NUM, i2c_cmd, pdMS_TO_TICKS(1));
     i2c_cmd_link_delete(i2c_cmd);
 }
 
 void drv_enable_digital() {    
     i2c_cmd_handle_t i2c_cmd = i2c_cmd_link_create();
-
+    i2c_master_start(i2c_cmd);
+    i2c_master_write_byte(i2c_cmd, (DRV2665_ADDR << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(i2c_cmd, DRV_REGISTER_2, true);    
+    i2c_master_write_byte(i2c_cmd, _STANDBY_FALSE, true);
+    i2c_master_stop(i2c_cmd);    
+    ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_MASTER_NUM, i2c_cmd, pdMS_TO_TICKS(1)));
+    i2c_cmd_link_delete(i2c_cmd);
+    
+    i2c_cmd = i2c_cmd_link_create();
     i2c_master_start(i2c_cmd);
     i2c_master_write_byte(i2c_cmd, (DRV2665_ADDR << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(i2c_cmd, DRV_REGISTER_1, true);    
     i2c_master_write_byte(i2c_cmd, INPUT_DIGITAL & GAIN_100V, true);
-    i2c_master_stop(i2c_cmd);
+    i2c_master_stop(i2c_cmd);    
+    ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_MASTER_NUM, i2c_cmd, pdMS_TO_TICKS(1)));
+    i2c_cmd_link_delete(i2c_cmd);
 
+    i2c_cmd = i2c_cmd_link_create();
     i2c_master_start(i2c_cmd);
     i2c_master_write_byte(i2c_cmd, (DRV2665_ADDR << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(i2c_cmd, DRV_REGISTER_2, true);    
-    i2c_master_write_byte(i2c_cmd, _STANDBY_FALSE & TIMEOUT_20MS & ENABLE_AUTO, true);
-    i2c_master_stop(i2c_cmd);
-
+    i2c_master_write_byte(i2c_cmd, _STANDBY_FALSE & TIMEOUT_20MS & ENABLE_OVERRIDE, true);
+    i2c_master_stop(i2c_cmd);    
+    ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_MASTER_NUM, i2c_cmd, pdMS_TO_TICKS(1)));
     i2c_cmd_link_delete(i2c_cmd);
+    vTaskDelay(2/portTICK_PERIOD_MS);
 }
 
 void spi_task(void *arg) {
@@ -184,44 +202,29 @@ void writer_task(void *pvParameters) {
         drv_write_fifo(buffer, write_size);
 
         // Calculate write delay ticks based on write_size and sample rate
-        TickType_t write_delay_ticks = (write_size * portTICK_PERIOD_MS * 1000) / SAMPLE_RATE;
+        TickType_t write_delay_ticks = (write_size / SAMPLE_RATE)* portTICK_PERIOD_MS * 1000;
         vTaskDelay(write_delay_ticks);          
     }
     vTaskDelete(NULL);
 }
 
-void app_main(void)
-{
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-    
-
+void app_main(void) {
 
     float frequency = 250.0; // Desired frequency in Hz
     sine_wave_size = (size_t)((SAMPLE_RATE / frequency) + 0.5); // Rounded up
 
     generate_sine_wave(frequency);
 
+    ESP_LOGI(TAG, "Connecting to peripherals");
     drv_init();
+    
+    ESP_LOGI(TAG, "DRV2665 initialized successfully");
     drv_enable_digital();
     xTaskCreatePinnedToCore(writer_task, "writer_task", configMINIMAL_STACK_SIZE * 4, NULL, 1, NULL, 0);
 
     xTaskCreate(spi_task, "spi_task", 4096, NULL, 10, NULL);
-    
+    while(1) {
+        vTaskDelay(1000*portTICK_PERIOD_MS);
+    }
 
 }
